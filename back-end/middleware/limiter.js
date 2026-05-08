771eead457603d526d924adbe4c4711084c9e500
kamelmaher <kamelmahersh@gmail.com>
2026-05-08 18:29:14 +0300

    rate limiter

--- back-end/middleware/limiter.js
+++ back-end/middleware/limiter.js
@@ -1,11 +1,16 @@
 const rateLimit = require("express-rate-limit");
-
+const { ERROR } = require("../data/statusText")
 const limiter = (hours = 12, max = 1) => {
     return rateLimit({
         windowMs: hours * 60 * 60 * 1000,
         max: max,
-        message: "محاولات كثيرة، حاول لاحقًا"
-    })
+        handler: (req, res) => {
+            return res.status(429).json({
+                status: ERROR,
+                data: "محاولات كثيرة، حاول لاحقًا"
+            });
+        }
+    });
 };
 
 module.exports = limiter
